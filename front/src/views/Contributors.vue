85268a68dde8b9e78f3f230dfd14107de19ff684
Gao <1149650442@qq.com>
2020-08-20 08:33:40 +0800

    Adjust style

---
+++ front/src/views/Contributors.vue
@@ -0,0 +1,100 @@
+<template>
+  <v-layout row>
+    <v-flex md3>
+      <v-app style="background-color:gery lighten-1;" class="ma-n4">
+        <v-container ml-5 mt-5>
+          <h4 class="mb-5">FILTERS</h4>
+          <v-text-field @change="getList()" v-model="condition.key" label="Search contributor's name" prepend-icon="search" class="ml-3 mr-12"></v-text-field>
+          <v-col cols="12" sm="6">
+            <v-date-picker @change="getList()" v-model="condition.dates" range landscape>
+               <v-tooltip bottom>
+                <template v-slot:activator="{ on, attrs }">
+                  <v-icon right
+                    color="primary"
+                    dark
+                    v-bind="attrs"
+                    v-on="on"
+                  >build</v-icon>
+                </template>
+                <span>Please select at least two dates</span>
+              </v-tooltip>
+            </v-date-picker>
+          </v-col>
+        </v-container>
+      </v-app>
+    </v-flex>
+    <v-flex md9>
+      <v-app style="background-color:white;" class="ma-n4">
+        <v-container>
+          <template>
+            <v-data-table 
+              :loading="loading"
+              :headers="headers"
+              :items="list"
+              :items-per-page="15"
+              class="mr-2"
+            >
+            <template v-slot:top>
+                <v-toolbar flat color="white">
+                    <v-toolbar-title>Contributors</v-toolbar-title>
+                    <v-divider class="mx-4" inset vertical></v-divider>
+                    <v-spacer></v-spacer>
+                </v-toolbar>
+            </template>
+            <template v-slot:[`item.avatar`]="{ item }">
+                <v-avatar size="30px"><img :src="[`/avatars/`]+item.author+[`.jpg`]"></v-avatar>
+            </template>
+            <template v-slot:[`item.link`]="{ item }">
+              <v-btn :href="item.link">
+                <v-icon dark>mdi-forward</v-icon>
+              </v-btn>
+            </template>
+            </v-data-table>
+          </template>
+        </v-container>
+      </v-app>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+import listApi from '@/api/prlist'
+export default {
+  data: () => ({
+    headers: [
+      { text: "Avatar", align: "left", sortable: false, value: "avatar" },
+      { text: "Name", value: "author"},
+      { text: "Title", value: "title"},
+      { text: "Commit time", value: "time" },
+      { text: "Check details", value: "link" },
+      // { text: "RATING", value: "rating" },
+    ],
+    list: [],
+    total: 0,
+    condition: {},
+    page: 1,
+    limit: 13796,
+    loading: false
+  }),
+  computed: {
+      dateRangeText () {
+        return this.dates.join(' ~ ')
+      },
+    },
+  created() {
+    this.condition = {};
+    this.getList();
+  },
+  methods: {
+    getList(page = 1) {
+      this.loading = true
+      this.page = page
+      listApi.getPrList(this.condition,this.page,this.limit).then(response => {
+        this.list = response.data.data.rows
+        this.total = response.data.data.total
+      })
+      this.loading = false
+    }
+  },
+};
+</script>
